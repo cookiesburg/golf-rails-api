d841f3d4479fdcb4b6cdec711c502a34f82a0a3c
Thomas DeFeo <thomasmdefeo@gmail.com>
2018-03-08 21:06:48 -0700

    create update destroyuser controller api endpoints

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,8 +1,35 @@
 module Api::V1
   class UsersController < ApplicationController
     def index
-      @users = User.all
+      @users = User.order("created_at DESC")
       render json: @users
     end
+
+    def create
+      @user = User.create(user_params)
+      render json: @user
+    end
+
+    def update
+      @user = User.find(params[:id])
+      @user.update_attributes(user_params)
+      render json: @user
+    end
+
+    def destroy
+      @user = User.find(params[:id])
+      if @user.destroy
+        head :no_content, status: :ok
+      else
+        render json: @user.errors, status: :unprocessable_entity
+      end
+    end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:name)
+    end
+
   end
 end
