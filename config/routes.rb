fcb53c9b4c0f75ecbbfd03e2f77f1c92bb734364
Thomas DeFeo <thomasmdefeo@gmail.com>
2018-03-14 10:41:35 -0600

    scores create and update routes functional

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,10 @@ Rails.application.routes.draw do
     namespace :v1 do
       get 'users/scores' => 'scores#index'
       get 'users/:id/scores' => 'users#scores'
-      resources :users do
-        resources :scores
-      end
+      resources :users
+      resources :scores
       resources :courses
     end
   end
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
